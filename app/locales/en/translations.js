e5ab1430080fe6b41508f76841d8b4c71bd0b5c8
woosungchu <woosungchu@gmail.com>
2017-03-24 17:55:11 +0900

    design index

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -9,9 +9,24 @@ export default {
   //
   // "key.with.interpolation": "Text with {{anInterpolation}}"
   'odototo': 'Odototo',
-  'degree': 'Degree',
-  'paint': 'Bleach color',
   'download': 'download',
+
+  'page-header': {
+    'title':'Good job!',
+    'small':'What\'s the next step?',
+  },
+
+  'paint': {
+    'title':'Bleach color',
+    'description':'Bleach color of the drawn image for redrawing',
+    'degree': 'Degree',
+  },
+
+  'blank': {
+    'title':'Fill in the blank!',
+    'description':'This is useful tool to make blank in the texts',
+  },
+
 
 
 
--- app/locales/ko/translations.js
+++ app/locales/ko/translations.js
@@ -9,9 +9,22 @@ export default {
   //
   // "key.with.interpolation": "Text with {{anInterpolation}}"
   'odototo': '오동통통',
-  'degree': '탈색 정도',
-  'paint': '색칠공부 도안제작',
   'download': '다운로드',
 
+  'page-header': {
+    'title':'참 잘했어요!',
+    'small':'다음은 무엇을 해볼까요?',
+  },
+
+  'paint': {
+    'title':'색칠공부 도안제작',
+    'description':'색깔이 그려진 색칠공부 도안에서 색을 빼드립니다!',
+    'degree': '탈색 정도',
+  },
+
+    'blank': {
+      'title':'빈칸 채우기',
+      'description':'글의 원하는 단어를 빈칸으로 만들어 버립니다!',
+    },
 
 };
