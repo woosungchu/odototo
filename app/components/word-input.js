0ccc4caf8a2995a4c4db34744bbabefe10ae0a5e
woosungchu <woosungchu@gmail.com>
2017-04-03 18:10:20 +0900

    add elementId into input and tab

--- app/components/word-input.js
+++ app/components/word-input.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  elementId: 'word-input',
   inputs: [],//[{word:'',answer:''},{word:'',answer:''}]
 
   addInput(data){
--- app/components/word-worksheet.js
+++ app/components/word-worksheet.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames : ['col-md-12'],
+
+  click(evt){
+    let target = evt.target,
+        sibling = target.parentElement.childNodes;
+
+    if(target.tagName === 'A'){
+      evt.preventDefault();
+
+    }
+  }
 });
