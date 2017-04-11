3ab6a80002bd2f3ce65efd5814fabaf820c122fd
woosungchu <woosungchu@gmail.com>
2017-04-11 16:30:34 +0900

    edit word-input

--- app/components/word-input.js
+++ app/components/word-input.js
@@ -7,79 +7,33 @@ export default Ember.Component.extend({
     this._super(...arguments);
   },
 
-  willRender(){
-    this._super(...arguments);
-    let inputs = this.get('inputs'),
-        last = inputs[inputs.length-1];
-
-    //init or last element is empty
-    if(!last || last.word || last.answer){
-      this.addInput();
-    }
-  },
-
-  didRender(){
-    this._super(...arguments);
-    let lis = document.getElementById('word-input')
-                    .getElementsByTagName("li"),
-        input = input = lis[lis.length-1].getElementsByTagName("input")[0];
-
-    input.focus();
-  },
-
   yield(){
     let inputs = this.get('inputs'),
-        outputs = inputs.slice(),//shallow copy
-        output = null;
-
-    //validate check
-    for(let i = 0 ; i < outputs.length; i++){
-        output = outputs[i];
-        if(!output.word && !output.answer){
-          outputs.splice(i,1);
-        }
-    }
+        outputs = inputs.slice();
 
     this.set('outputs',outputs);
   },//end yield
 
-  sync(){
-    let lis = document.getElementById('word-input')
-                    .getElementsByTagName("li"),
-        inputs = this.get('inputs'),
-        arr = [],
-        input,data;
-
-    for(let i = 0; i < lis.length; i++){
-      input = lis[i].getElementsByTagName("input");
-      data ={word:'',answer:''};
-
-      data.word = input[0].value;
-      data.answer = input[1].value;
-
-      arr.push(data);
-    }
-
-    inputs.setObjects(arr);
-    this.yield();
-  },
-
-  addInput(data){
-    let inputs = this.get('inputs'),
-        record = data || {word:'',answer:''};
-
-    inputs.pushObject(record);
-  },
-
   keyPress(evt) {
     if (evt.which === 13) {
-      this.sync();
+      this.send('addrow');
     }
   },
 
   actions : {
     addrow(){
-      this.sync();
+      let inputs = this.get('inputs'),
+          quiz = document.getElementById('word-input')
+                        .getElementsByTagName("input"),
+          data = null;
+
+      data = {
+        word:quiz[0].value,
+        answer:quiz[1].value
+      };
+
+      inputs.pushObject(data);
+      this.yield();
     },//addrow
 
     removerow(idx){
